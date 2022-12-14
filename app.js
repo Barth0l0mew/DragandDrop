c9236e42322ebd646016e0e8e79704a8f75c9f0d
barth0l0mew <barth0l0mew@mail.ru>
2022-12-14 21:18:11 +0300

    js_dragdrop

--- app.js
+++ app.js
@@ -2,18 +2,31 @@ const item=document.querySelector('.item')
 const placeholders=document.querySelectorAll('.placeholder')
 for (const placeholder of placeholders){
     placeholder.addEventListener('dragover',dragover)
+    placeholder.addEventListener('dragenter',dragenter)
+    placeholder.addEventListener('dragleave',dragleave)
+    placeholder.addEventListener('drop',dragdrop)
 }
 item.addEventListener('dragstart',dragstart)
 item.addEventListener('dragend',dragend)
 function dragover (event){
     event.preventDefault();
 }
+function dragenter (event){
+    event.target.classList.add('hovered')
+}
+function dragleave (event){
+    event.target.classList.remove('hovered')
+}
+function dragdrop (event){
+    event.target.classList.remove('hovered')
+    event.target.append(item)
+
+}
 function dragstart(event){
     event.target.classList.add('hold')
+    setTimeout(()=>{event.target.classList.add('hide')},0)
+    console.log('drag star')
 }
 function dragend(event){
-    event.target.classList.remove('hold')
-
-
+    event.target.classList.remove('hold','hide')
 }
-console.log (item)
